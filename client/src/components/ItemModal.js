cf4414468e09602e88e34b33eafce26d3cbcd695
James Fang <jtotheamesfang@gmail.com>
2020-04-25 16:16:59 -0500

    connected to backend

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -6,7 +6,6 @@ import {
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import { addItem } from '../actions/itemActions';
-import { v4 as uuid } from 'uuid';
 
 class ItemModal extends Component {
   state = {
@@ -30,7 +29,6 @@ class ItemModal extends Component {
     e.preventDefault();
 
     const newItem = {
-      id: uuid(),
       name: this.state.name
     }
 
